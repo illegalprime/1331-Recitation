25da8a931da0a6686e6da038edcb056782e1e695
Michael Eden <themichaeleden@gmail.com>
2015-03-24 19:20:24 -0400

    Broke up 350 character one-liner

---
+++ Bing/Thing.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+
+public class Thing<Integer> {
+    public static void main(String[] args) {
+
+        Thing<String> thing = new Thing<String>();
+
+        // Set<String> onlyUnique = new HashSet<>();
+        // onlyUnique.add("String");
+        //
+        // ArrayList<Integer> list = new ArrayList<>();
+        // list.add(1);
+        // list.add(2);
+        // list.add(3);
+        // System.out.println(list.get(0) + 89);
+        //
+        // for (int data : list) {
+        //     System.out.println(data);
+        // }
+        //
+        // Map<String, String> map = new HashMap<String, String>();
+        // map.put("Maps", "Album");
+        //
+        // System.out.println(map.get("Maps")); // "Album"
+        //
+        // map.put("Maps", "R cool");
+
+
+    }
+}
--- Bing/src/Bing.java
+++ Bing/src/Bing.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -9,11 +11,12 @@ public class Bing {
 
     public static void main(String[] args) {
 
-        System.out.println("██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ████████╗ ██████╗     ██████╗ ██╗███╗   ██╗ ██████╗ ██╗\n" +
+        System.out.println(
+         "██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ████████╗ ██████╗     ██████╗ ██╗███╗   ██╗ ██████╗ ██╗\n" +
         "██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ╚══██╔══╝██╔═══██╗    ██╔══██╗██║████╗  ██║██╔════╝ ██║\n" +
-                "██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗         ██║   ██║   ██║    ██████╔╝██║██╔██╗ ██║██║  ███╗██║\n" +
+         "██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗         ██║   ██║   ██║    ██████╔╝██║██╔██╗ ██║██║  ███╗██║\n" +
         "██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝         ██║   ██║   ██║    ██╔══██╗██║██║╚██╗██║██║   ██║╚═╝\n" +
-                "╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗       ██║   ╚██████╔╝    ██████╔╝██║██║ ╚████║╚██████╔╝██╗\n" +
+         "╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗       ██║   ╚██████╔╝    ██████╔╝██║██║ ╚████║╚██████╔╝██╗\n" +
         " ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝       ╚═╝    ╚═════╝     ╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝\n");
 
         Scanner keyboard = new Scanner(System.in);
@@ -30,68 +33,68 @@ public class Bing {
          *************************************************************/
 
         // Filtering
-//        List<Link> filtered = new LinkedList<>();
-//
-//        for (Link link : searchResults) {
-//            if (!link.getTitle().contains("Google")) {
-//                filtered.add(link);
-//            }
-//        }
-//
-//        // Ordering
-//        filtered.sort(new Comparator<Link>() {
-//            @Override
-//            public int compare(Link o1, Link o2) {
-//                boolean o1HasBing = o1.getTitle().contains("Bing");
-//                boolean o2HasBing = o2.getTitle().contains("Bing");
-//
-//                if (o1 == o2) {
-//                    return o1.getTitle().compareTo(o2.getTitle());
-//                }
-//                else if (o1HasBing) {
-//                    return -1;
-//                }
-//                else {
-//                    return 1;
-//                }
-//            }
-//        });
-//
-//        // Printing
-//        for (Link link : filtered) {
-//            System.out.println(link.getTitle());
-//        }
+        List<Link> filtered = new LinkedList<>();
+
+        for (Link link : searchResults) {
+            if (!link.getTitle().contains("Google")) {
+                filtered.add(link);
+            }
+        }
+
+        // Ordering
+        filtered.sort(new Comparator<Link>() {
+            @Override
+            public int compare(Link o1, Link o2) {
+                boolean o1HasBing = o1.getTitle().contains("Bing");
+                boolean o2HasBing = o2.getTitle().contains("Bing");
+
+                if (o1 == o2) {
+                    return o1.getTitle().compareTo(o2.getTitle());
+                }
+                else if (o1HasBing) {
+                    return -1;
+                }
+                else {
+                    return 1;
+                }
+            }
+        });
+
+        // Printing
+        for (Link link : filtered) {
+            System.out.println(link.getTitle());
+        }
 
         /*************************************************************
          * Now labmdas!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! *
          *************************************************************/
 
-        List<Link> functionalResults = Scraper.getGoogleLinks(query);
-
-        // Filtering
-        functionalResults = functionalResults.stream()
-                .filter(s -> !s.getTitle().toLowerCase().contains("google"))
-                .sorted((l1, l2) -> {
-                    boolean o1HasBing = l1.getTitle().contains("Bing");
-                    boolean o2HasBing = l2.getTitle().contains("Bing");
-
-                    if (l1 == l2) {
-                        return l1.getTitle().compareTo(l2.getTitle());
-                    }
-                    else if (o1HasBing) {
-                        return -1;
-                    }
-                    else {
-                        return 1;
-                    }
-                })
-                .collect(Collectors.toList());
-
-        if (functionalResults.isEmpty()) {
-            System.out.println("Could not find anything matching '" + query + "'");
-        }
-        else {
-            functionalResults.stream().forEach(System.out::println);
-        }
+//        List<Link> functionalResults = Scraper.getGoogleLinks(query);
+//
+//        // Filtering
+//        functionalResults = functionalResults.stream()
+//                .filter(s -> !s.getTitle().toLowerCase().contains("google"))
+//                .sorted((l1, l2) -> {
+//                    boolean o1HasBing = l1.getTitle().contains("Bing");
+//                    boolean o2HasBing = l2.getTitle().contains("Bing");
+//
+//                    if (l1 == l2) {
+//                        return l1.getTitle().compareTo(l2.getTitle());
+//                    }
+//                    else if (o1HasBing) {
+//                        return -1;
+//                    }
+//                    else {
+//                        return 1;
+//                    }
+//                })
+//                .collect(Collectors.toList());
+//
+//        if (functionalResults.isEmpty()) {
+//            System.out.println("Could not find anything matching '" + query + "'");
+//        }
+//        else {
+//            functionalResults.stream().forEach(System.out::println);
+//        }
     }
 }
